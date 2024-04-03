007c7e23018ddb6d8d0e45ac911541e9422ef427
LinardsLiepenieks <linardsliepenieks@gmail.com>
2024-04-03 17:33:55 +0300

    about-slides

--- yolo_community/landing/static/landing/js/section-snap.js
+++ yolo_community/landing/static/landing/js/section-snap.js
@@ -1,38 +1,88 @@
 
+
 $(document).ready(function () {
-	var navbar = $("nav");
-	var sections = $("section");
-	var aboutSlides = $(".about-us-slide");
+	var aboutSlides = $(".about-slide-img-container img");
+	var aboutSlidesContent = $(".about-slide-content-box");
+	var curtainLeft = $("#curtainLeft");
+	var curtainRight = $("#curtainRight");
+	var readBar = $("#readBar");
+	var readBarMaxLength = aboutSlides.length;
+	var readBarLength = 0;
+	readBar.css("width", "0%");
 	aboutSlides.eq(0).addClass("active");
+	aboutSlidesContent.eq(0).addClass("active");
 	var curSection = 0;
 	var aboutSlide = 0;
 	var isAnimating = false;
+
 	
 	$("#about-us-right-btn > img").css("transition", "all 0.4s ease");
 	$("#about-us-right-btn > img").css("transform", "rotate(-90deg)");
 	$("#about-us-left-btn > img").css("transition", "all 0.4s ease");
 	$("#about-us-left-btn > img").css("transform", "rotate(90deg)");
-	$("#about-us-left-btn > img").css("opacity", "0");
+	$("#about-us-left-btn > img").css("opacity", "0.2");
+	$("#about-us-left-btn").css("cursor", "default");
+
 	
 	function transitionAboutSlide(scrollDirection) {
-		// Dissolving animation between about slides - middle section
 		var currentSlide = aboutSlides.eq(aboutSlide);
+		var currentSlideContent = aboutSlidesContent.eq(aboutSlide);
 		let nextSlide = aboutSlides.eq(aboutSlide + scrollDirection);
-		if (aboutSlide == 0 && scrollDirection == 1) {
-			$("#about-us-right-btn > img").css("opacity", "0");
-		} else if (aboutSlide == 1 && scrollDirection == -1) {
+		let nextSlideContent = aboutSlidesContent.eq(aboutSlide + scrollDirection);
+		let curtain;
+		let anim;
+		isAnimating = true;
+		if (aboutSlide+1 >= aboutSlides.length-1 && scrollDirection == 1) {
+			$("#about-us-right-btn > img").css("opacity", "0.2");
+			$("#about-us-right-btn > img").css("cursor", "default");
+		} else {
 			$("#about-us-right-btn > img").css("opacity", "1");
+			$("#about-us-right-btn > img").css("cursor", "pointer");
 		}
-		if (aboutSlide == aboutSlides.length - 2 && scrollDirection == 1) {
+		if (aboutSlide >= 0  && scrollDirection == 1) {
 			$("#about-us-left-btn > img").css("opacity", "1");
-		} else if (aboutSlide == aboutSlides.length - 1 && scrollDirection == -1) {
-			$("#about-us-left-btn > img").css("opacity", "0");
+			$("#about-us-left-btn").css("cursor", "pointer");
+		} else if (aboutSlide <= 1 && scrollDirection == -1) {
+			$("#about-us-left-btn > img").css("opacity", "0.2");
+			$("#about-us-left-btn").css("cursor", "default");
 		}
 
-		currentSlide.removeClass("active");
-		aboutSlide += scrollDirection;
-		console.log(nextSlide)
-		nextSlide.addClass("active");
+		if (scrollDirection == 1)
+		{
+			curtain = curtainLeft;
+			anim = {width: '100%'};
+			animOut = {width: '0%', left: '100%'};
+		}
+		else if(scrollDirection == -1)
+		{
+			curtain = curtainRight;
+			anim = {width: '100%', right: '0'};
+			animOut = {width: '0%', right: '100%'};
+
+		}
+		readBarLength += scrollDirection;
+		let readBarInc = (100/(readBarMaxLength-1)) * (readBarLength)
+		console.log(readBarInc)
+		readBar.css("width", readBarInc+"%")
+		curtain.animate(anim, {
+			duration: 400,
+			easing: 'swing', // Use swing easing function
+			complete: function() {
+				currentSlide.removeClass("active");
+				currentSlideContent.removeClass("active");
+				aboutSlide += scrollDirection;
+				nextSlide.addClass("active");
+				nextSlideContent.addClass("active");
+				curtain.animate(animOut, {
+					duration: 400,
+					easing: 'swing', // Use swing easing function
+					complete: function() {
+						curtain.removeAttr('style');
+						isAnimating = false;
+					}
+				});
+			}
+		});
 			
 	}
 
